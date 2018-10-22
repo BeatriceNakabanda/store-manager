bd50f6f4d3715da58f34df7077d6544bfdd77dd8
EmmellaBeatrice <37703341+EmmellaBeatrice@users.noreply.github.com>
2018-10-22 22:36:37 +0300

    Update test_product.py
    improved product tests

--- api/tests/test_product.py
+++ api/tests/test_product.py
@@ -4,49 +4,40 @@ from api.models.products import Product
 
 class TestProduct(unittest.TestCase):
     def setUp(self):
-        self.product = Product(1, 5, 50000, "Slim Mom Jeans","Tue, 16 Oct 2018 18:42:38 GMT")
+        self.product = Product(1, 1, 2000, "Shirt","Tue, 10/11/28 09:49")
 
     def test_product_id(self):
-        # Tests that the prodct_id is equal to the given id
         self.assertEqual(self.product.product_id, 1, "product_id must be 1")
-        self.product.product_id = 8
-        self.assertEqual(self.product.product_id, 8, "product_id is now 8")
+        self.product.product_id = 6
+        self.assertEqual(self.product.product_id, 6, "product_id is now 6")
 
     def test_product_id_type(self):
-        # Tests the datatype of the product id
         self.assertNotIsInstance(self.product.product_id, str)
         self.assertIsInstance(self.product.product_id, int)
 
     def test_product_quantity(self):
-        # Tests that the product quantity is equal to the given quantity
-        self.assertEqual(self.product.quantity, 5, "Quantity should be 5")
+        self.assertEqual(self.product.quantity, 8, "Quantity is 8")
 
     def test_product_quantity_type(self):
-        # Tests the datatype of the product quantity
         self.assertIsInstance(self.product.quantity, int)
         self.assertNotIsInstance(self.product.quantity, str)
 
     def test_product_price(self):
-        # Tests that the price is equal to the given product price
-        self.assertEqual(self.product.price, 50000, "price must be 50000")
+        self.assertEqual(self.product.price, 3000, "price is 3000")
 
     def test_price_datatype(self):
-        # Tests the price of the product
         self.assertIsInstance(self.product.price, int)
         self.assertNotIsInstance(self.product.price, float)
         self.assertNotIsInstance(self.product.price, str)
 
     def test_product_name(self):
-        # Tests that the product name is equal to the given product name
-        self.assertEqual(self.product.product_name, "Slim Mom Jeans")
-        self.product.product_name = "covert jeans"
-        self.assertEqual(self.product.product_name, "covert jeans",
-                         "product name is now covert jeans")
+        self.assertEqual(self.product.product_name, "Omo")
+        self.product.product_name = "Nomi"
+        self.assertEqual(self.product.product_name, "Nomi",
+                         "product name is now Nomi")
 
     def test_date_added(self):
-        # Tests that the date is equal to the given date
-        self.assertEqual(self.product.date_added,'Tue, 16 Oct 2018 18:42:38 GMT')
+        self.assertEqual(self.product.date_added,'Mon, 12/11/2016 16:30')
 
     def test_class_instance(self):
-        # Tests that the defined object is an instance of the Product class
         self.assertIsInstance(self.product, Product)
