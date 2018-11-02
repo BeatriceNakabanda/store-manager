fe95b34936a51c89e993d794756be588fa7cc154
Beatrice <nakabandabeatrice@gmail.com>
2018-11-02 10:10:52 +0300

    add database to travis

--- database/db.py
+++ database/db.py
@@ -10,6 +10,12 @@ class Database:
         self.password = "admin"
         self.port = 5432
 
+        # self.db = "deivon7io7dggi"
+        # self.user = "bzczspnlfokryx"
+        # self.host = "ec2-107-22-189-136.compute-1.amazonaws.com"
+        # self.password = "a9731e743be4e474fba0cd5bee49ff57f136d35956c58c41f298a5e662e7e9ac"
+        # self.port = 5432
+
         try:
             self.connection = psycopg2.connect(dbname=self.db,user=self.user,\
                                 password=self.password, host=self.host,port=self.port)
